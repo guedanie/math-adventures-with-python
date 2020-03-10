35e679795cf7945a6cce227c11155c2f1e8b15fb
guedanie <danguerrero93@gmail.com>
2020-03-09 22:43:53 -0500

    First commit star function

---
+++ mytutrtle_star.py
@@ -0,0 +1,16 @@
+from turtle import *
+shape('turtle')
+speed(10)
+
+def star(length):
+    for i in range(5):
+        forward(length)
+        right(120)
+        forward(length)
+        right(72 - 120)
+    
+length = 5        
+for i in range(60):
+    star(length)
+    right(5)
+    length += 5
